b10e8c319f9ebd8d6e3cfc5b9c3d1ed077e9f361
Billy Manners <hwilliamfmanners@gmail.com>
2020-10-03 10:31:56 +0100

    New tracking class

---
+++ TrackYourRun/speedDistanceTracking.swift
@@ -0,0 +1,17 @@
+//
+//  speedDistanceTracking.swift
+//  TrackYourRun
+//
+//  Created by Manners, William (Coll) on 03/10/2020.
+//  Copyright © 2020 Manners, William (Coll). All rights reserved.
+//
+
+import Foundation
+
+class speedDistanceTracking{
+    var distance = 0
+    var averageSpeed = 0
+    
+    
+    
+}
